079a8334e93a8adee20d3cbe6110eb4514836b20
Eliseo Jimenez <eliseojimenezjimenez23@gmail.com>
2025-12-18 22:32:34 -0400

    feat: Adicion de NvimTree

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,5 @@
 vim.keymap.set("n", "gb", ":bn<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "gB", ":bp<CR>", { noremap = true, silent = true })
+
+-- Para poder cerrar y abrir NvimTree
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
